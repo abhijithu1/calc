7808106323548c3aa1179289f7c4316906ef0f40
abhijithu1 <abhiunni9656@gmail.com>
2022-11-19 21:22:21 +0530

    Made a black background

--- calculator.py
+++ calculator.py
@@ -7,6 +7,15 @@ root = Tk()
 #Naming the title of the app
 root.title("Calculator")
 
+#Setting the size of the window
+root.geometry("300x300")
+
+#Just set an image with photoimage lib
+bg = PhotoImage(file="black.v1.png")
+
+
+#Creating a label to display image
+label1 = Label(root,image=bg).pack()
 
 
 #The mainloop thing prevents closing of the window
