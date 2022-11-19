5a59e5e0336df5f4af1f453df3caaeba60e6a3dd
abhijithu1 <abhiunni9656@gmail.com>
2022-11-19 20:29:55 +0530

    Just created a window of calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#Importing the tkinter library
+from tkinter import *
+
+#Defining a window
+root = Tk()
+
+#Naming the title of the app
+root.title("Calculator")
+
+
+
+#The mainloop thing prevents closing of the window
+root.mainloop()
